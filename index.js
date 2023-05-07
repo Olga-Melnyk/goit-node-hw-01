fd403462303681b1e69798d90d313dda1273ad0b
Olga-Melnyk <3717129@ukr.net>
2023-05-07 19:05:17 +0300

    add functions

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log("Hello");
+const contactsService = require("./contacts");
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const allContacts = await contactsService.listContacts();
+      console.log(allContacts);
+      break;
+
+    case "get":
+      const oneContacts = await contactsService.getContactById(id);
+      console.log(oneContacts);
+      break;
+
+    case "add":
+      const newContact = await contactsService.addContact(name, email, phone);
+      console.log(newContact);
+      break;
+
+    case "remove":
+      const removeContact = await contactsService.removeContact(id);
+      console.log(removeContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+// invokeAction({
+//   action: "remove",
+//   id: "KVa2l0ekJCT_tPL-BeM2C",
+// });
